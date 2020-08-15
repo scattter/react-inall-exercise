0b6e0fe7a4a029fe12fb2884b8a4dcda6788b43a
scattter <413365742@qq.com>
2020-08-15 18:23:38 +0800

    [zk] 计算器功能实现

--- src/app/Calculator.js
+++ src/app/Calculator.js
@@ -5,6 +5,35 @@ import Header from '../component/Header';
 import Button from '../component/Button';
 
 export default class Calculator extends Component {
+    state = {
+        display: '',
+    }
+
+    calculatorDis = (event) => {
+        const size = this.state.display.length;
+        const last = this.state.display[size - 1];
+
+        if (size === 0 && !event.target.value.match('[0-9]')) {
+            alert('输入错误');
+        } else if (size > 0 && !last.match('[0-9]') && !event.target.value.match('[0-9]')) {
+            alert('输入错误,请输入数字');
+        } else {
+            this.setState({
+                display: this.state.display + event.target.value,
+            })
+        }
+        if (event.target.value === 'clear') {
+            this.setState({
+                display: '',
+            })
+        }
+        if (event.target.value === '=') {
+            this.setState({
+                display: this.state.display + '=' + eval(this.state.display),
+            })
+        }
+
+    }
     render() {
         return (
             <div className='calculator'>
@@ -14,13 +43,15 @@ export default class Calculator extends Component {
                     <div className='main'>
                         <br></br>
                         <br></br>
-                        <div className='display'></div>
+                        <div className='display'>
+                            <p>{this.state.display}</p>
+                        </div>
                         <div className='btn'>
-                            <Button button={['+', '-', 'x']} order='yellowLine'></Button>
-                            <Button button={[7, 8, 9]} order='originLine'></Button>
-                            <Button button={[4, 5, 6]} order='originLine'></Button>
-                            <Button button={[1, 2, 3]} order='originLine'></Button>
-                            <Button button={[0, 'clear', '=']} order='fourLine'></Button>
+                            <Button button={['+', '-', '*']} order='yellowLine' cal={this.calculatorDis}></Button>
+                            <Button button={[7, 8, 9]} order='originLine' cal={this.calculatorDis}></Button>
+                            <Button button={[4, 5, 6]} order='originLine' cal={this.calculatorDis}></Button>
+                            <Button button={[1, 2, 3]} order='originLine' cal={this.calculatorDis}></Button>
+                            <Button button={[0, 'clear', '=']} order='fourLine' cal={this.calculatorDis}></Button>
                         </div>
                     </div>
                     <Link to='/'>回到主页</Link>
--- src/component/Button.js
+++ src/component/Button.js
@@ -5,13 +5,12 @@ export default class Button extends Component {
     render() {
         const buttons = this.props.button;
         const btnClass = this.props.order;
-        // console.log(this.props.order);
-        console.log(btnClass);
+        const clickDis = this.props.cal;
         return (
             <div className='btn-component'>
-                <input type='button' className={btnClass} value={buttons[0]}></input>
-                <input type='button' className={btnClass} value={buttons[1]}></input>
-                <input type='button' className={btnClass} value={buttons[2]}></input>
+                <input type='button' className={btnClass} value={buttons[0]} onClick={clickDis}></input>
+                <input type='button' className={btnClass} value={buttons[1]} onClick={clickDis}></input>
+                <input type='button' className={btnClass} value={buttons[2]} onClick={clickDis}></input>
             </div>
         )
     }
